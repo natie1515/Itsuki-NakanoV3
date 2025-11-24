cba1b0d9bc3b96e63a646865d1e83502bc72be79
ᥣᥱ᥆xі𝗍᥆. <erenxz01@gmail.com>
2025-11-24 00:01:33 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -23,7 +23,7 @@ let syt = 'https://youtube.com'
 global.docs = ['doc1', 'doc2', 'doc3']
 global.nomorown = '573187418668@s.whatsapp.net'
 // global.logo = 'https://raw.githubusercontent.com/WillZek/Storage-CB2/main/images/54dfe27a73cf.jpg'
-global.iconorcanal = 'https://raw.githubusercontent.com/WillZek/Storage-CB2/main/images/54dfe27a73cf.jpg'
+// global.iconorcanal = 'https://raw.githubusercontent.com/WillZek/Storage-CB2/main/images/54dfe27a73cf.jpg'
 global.metanombre = 'Meta AI'
 
 global.idchannel = '120363403726798403@newsletter'
