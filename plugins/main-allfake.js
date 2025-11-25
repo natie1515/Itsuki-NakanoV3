7be5ff6b61c9b27738a5b4200bc4395c72cd95c6
ᥣᥱ᥆xі𝗍᥆. <erenxz01@gmail.com>
2025-11-25 17:02:26 -0400

    Agregando Variables dinámicas

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -5,10 +5,12 @@ import moment from 'moment-timezone'
 
 var handler = m => m
 handler.all = async function (m) { 
+// Variables de canales
 global.canalIdM = ["120363404434164076@newsletter", "120363404434164076@newsletter"]
 global.canalNombreM = ["꒰ ❄️ ITՏᑌKI ᑎᗩKᗩᑎO-ᐯ3 ᑌᑭᗪᗩTᗴ ☃️ ꒱", "𝆺𝅥 𝆭 ִ ֗ ❄ 𝐋𝐚𝐬 𝐐𝐮𝐢𝐧𝐭𝐢𝐥𝐥𝐢𝐬𝐚𝐬-𝐍𝐨𝐯𝐞𝐝𝐚𝐝 📢 ┆ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥-𝐈𝐧𝐟𝐨 ☃️ ˚₊‧"]
 global.channelRD = await getRandomChannel()
 
+// Fecha y hora
 global.d = new Date(new Date + 3600000)
 global.locale = 'es'
 global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
@@ -17,16 +19,19 @@ global.mes = d.toLocaleDateString('es', {month: 'long'})
 global.año = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
+// Enlaces
 var canal = 'https://whatsapp.com/channel/0029VbBBXTr5fM5flFaxsO06'  
+var comunidad = 'https://chat.whatsapp.com/BXxWuamOOE4K9eKC623FIO'
 var git = 'https://github.com/leoxito'
 var github = 'https://github.com/leoxito/Itsuki-NakanoV2'
 var correo = 'xzzysultra@gmail.com'
-global.redes = [canal, git, github, correo].getRandom()
+global.redes = [canal, comunidad, git, github, correo].getRandom()
 
+// Información del usuario y stickers
 global.nombre = m.pushName || 'User-MD'
 global.packsticker = ``
 
-// SOLO CAMBIÉ LAS IMÁGENES - Todo lo demás igual
+// Iconos random
 global.iconos = [
   'https://cdn.russellxz.click/a015cecd.jpg',
   'https://cdn.russellxz.click/d112a400.jpg',
@@ -40,13 +45,35 @@ global.iconos = [
   'https://cdn.russellxz.click/f46d62d3.jpg',
   'https://cdn.russellxz.click/36caddb4.jpg'
 ]
-
-// Seleccionar imagen random al azar
 global.icono = global.iconos[Math.floor(Math.random() * global.iconos.length)]
 
-global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+// Variables globales específicas solicitadas
+global.wm = '© 𝐋𝐞𝐨  𝐗𝐬𝐳𝐲'
+global.wm3 = '⫹⫺ 𝙈𝙪𝙡𝙩𝙞-𝘿𝙚𝙫𝙞𝙘𝙚 💻'
+global.author = '👑 ᗰᗩᗪᗴ ᗷY ᒪᗴO 🧃'
+global.dev = '© 𝗲𝗰𝗵𝗼 𝗽𝗼𝗿 𝗹𝗲𝗼 👑'
+global.textbot = 'Itsuki-Nakano|IAV3 Leo'
+global.etiqueta = '@Leo Xzsy'
+global.gt = '© 𝐂𝐫𝐞𝐚𝐝𝐨 𝐏𝐨𝐫 𝐋𝐞𝐨𝐃𝐞𝐯 𝐈𝐭𝐬𝐮𝐤𝐢-𝐂𝐡𝐚𝐧 𝐓𝐡𝐞 𝐁𝐞𝐬𝐭 𝐁𝐨𝐭𝐬 𝐎𝐟 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🤖👑'
+global.me = '🌨️ 𝙸𝚃𝚂𝚄𝙺𝙸 𝙽𝙰𝙺𝙰𝙽𝙾 𝙼𝙴𝚆 𝚄𝙿𝙳𝙰𝚃𝙴 ☃️'
 
-// Adaptado rcanal para usar global.icono random
+// Contact message
+global.fkontak = { 
+  key: { 
+    participants: "0@s.whatsapp.net", 
+    "remoteJid": "status@broadcast", 
+    "fromMe": false, 
+    "id": "Halo" 
+  }, 
+  "message": { 
+    "contactMessage": { 
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+    }
+  }, 
+  "participant": "0@s.whatsapp.net" 
+}
+
+// Mensaje de canal adaptado con las variables globales
 global.rcanal = { 
   contextInfo: { 
     isForwarded: true, 
@@ -56,19 +83,24 @@ global.rcanal = {
       newsletterName: channelRD.name 
     }, 
     externalAdReply: { 
-      title: botname, 
-      body: dev, 
+      title: global.botname, 
+      body: global.dev, // Usando global.dev
       mediaUrl: null, 
       description: null, 
       previewType: "PHOTO", 
-      thumbnailUrl: global.icono, // Usando global.icono random
-      sourceUrl: redes, 
+      thumbnailUrl: global.icono,
+      sourceUrl: global.redes, 
       mediaType: 1, 
       renderLargerThumbnail: false 
     }, 
     mentionedJid: null 
   }
 }
+
+// Otras variables útiles que pueden necesitarse
+global.listo = '*Aqui tiene*'
+global.moneda = 'Yenes'
+global.prefix = ['.', '!', '/', '#', '%']
 }
 
 export default handler
@@ -78,8 +110,15 @@ return list[Math.floor(Math.random() * list.length)]
 }
 
 async function getRandomChannel() {
-let randomIndex = Math.floor(Math.random() * canalIdM.length)
-let id = canalIdM[randomIndex]
-let name = canalNombreM[randomIndex]
+let randomIndex = Math.floor(Math.random() * global.canalIdM.length)
+let id = global.canalIdM[randomIndex]
+let name = global.canalNombreM[randomIndex]
 return { id, name }
+}
+
+// Extender el array para el método getRandom si no existe
+if (!Array.prototype.getRandom) {
+Array.prototype.getRandom = function() {
+return this[Math.floor(Math.random() * this.length)]
+}
 }
