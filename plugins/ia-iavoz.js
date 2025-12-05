58b42e16642c5885b5289d4917078734847e239c
Jarednnnn <patylachina3@gmail.com>
2025-12-05 03:07:39 -0500

    Update ia-iavoz.js

--- plugins/ia-iavoz.js
+++ plugins/ia-iavoz.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
   if (!text) return m.reply('Escribe algo')
   
-  const apiKey = '62d734ca543945338b343d3b6a88776f'
-  const url = `http://api.voicerss.org/?key=${apiKey}&hl=es-es&v=Mia&src=${encodeURIComponent(text)}`
+  // API de Google TTS (gratis, sin límites)
+  const url = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=es&q=${encodeURIComponent(text)}`
   
   try {
     const res = await fetch(url)
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text }) => {
     }, { quoted: m })
     
   } catch (e) {
-    m.reply('Error: ' + e.message)
+    m.reply('Error')
   }
 }
 
