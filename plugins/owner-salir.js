15a2c4415a3fd741dae905df7a17cd268a9b4e04
Jarednnnn <patylachina3@gmail.com>
2025-12-05 00:35:12 -0500

    Update owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,15 +1,22 @@
 let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-let chat = global.db.data.chats[m.chat]
-chat.welcome = false
-await conn.reply(id, `> *Me Despido Adios mis bby ✨️*`) 
-await conn.groupLeave(id)
-try {  
-chat.welcome = true
-} catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
-}}
+    let id = text ? text : m.chat
+    let chat = global.db.data.chats[m.chat]
+    
+    // Desactivar temporalmente las bienvenidas
+    chat.welcome = false
+
+    await conn.reply(id, `> ⓘ Orden ejecutada: Me retiro del grupo, mi señor.`)
+    await conn.groupLeave(id)
+
+    try {
+        // Reactivar bienvenidas tras la salida
+        chat.welcome = true
+    } catch (e) {
+        await m.reply('> ⓘ Error al procesar la salida, mi señor.')
+        console.log(e)
+    }
+}
+
 handler.command = ['salir','leavegc','salirdelgrupo','leave']
 handler.group = true
 handler.rowner = true
